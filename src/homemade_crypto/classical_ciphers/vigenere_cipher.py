ad326fa69a38b789daea1d31fbd9edbd3b19f848
sondre <sondre.hal@gmail.com>
2019-08-29 13:44:20 +0200

    Complete Vigenere

--- src/homemade_crypto/classical_ciphers/vigenere_cipher.py
+++ src/homemade_crypto/classical_ciphers/vigenere_cipher.py
@@ -1,4 +1,5 @@
 import random
+import itertools
 
 ASCII = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 NOR = 'ABCDEFGHIJKLMNOPQRSTUVWXYZÆÅ'
@@ -7,7 +8,8 @@ NOR = 'ABCDEFGHIJKLMNOPQRSTUVWXYZÆÅ'
 class VigenereCipher:
 
     def __init__(self, key_space):
-        self._key_space = key_space
+        self._key_space = list(key_space)
+        self._m = len(key_space)
 
     @classmethod
     def norwegian_vigenere(cls) -> "VigenereCipher":
@@ -22,3 +24,27 @@ class VigenereCipher:
         pick_space = list(self._key_space)
         random.shuffle(pick_space)
         return "".join(pick_space[:key_length])
+
+    def encrypt(self, key, plain_text: str) -> str:
+        key = key.upper()  # make sure key does not contain lower case
+        letters = list(plain_text.upper())
+        key_letter_pair = list(zip(letters, itertools.cycle(key)))
+        cipher_list = []
+        for letter, key_letter in key_letter_pair:
+            letter_index = self._key_space.index(letter)
+            key_letter_index = self._key_space.index(key_letter)
+            cipher_letter_index = (letter_index + key_letter_index) % self._m
+            cipher_list.append(self._key_space[cipher_letter_index])
+        return "".join(cipher_list)
+
+    def decrypt(self, key, cipher_text):
+        key = key.upper()  # make sure key does not contain lower case
+        letters = list(cipher_text.upper())
+        key_letter_pair = list(zip(letters, itertools.cycle(key)))
+        cipher_list = []
+        for letter, key_letter in key_letter_pair:
+            letter_index = self._key_space.index(letter)
+            key_letter_index = self._key_space.index(key_letter)
+            cipher_letter_index = (letter_index - key_letter_index) % self._m
+            cipher_list.append(self._key_space[cipher_letter_index])
+        return "".join(cipher_list)
--- tests/homemade_crypto/classical_ciphers/test_vigenereCipher.py
+++ tests/homemade_crypto/classical_ciphers/test_vigenereCipher.py
@@ -11,6 +11,17 @@ class TestVigenereCipher(TestCase):
         self.assertIsInstance(key, str)
         self.assertTrue(len(key) > 0)
 
+    def test_encrypt(self):
+        vigenere = VigenereCipher.norwegian_vigenere()
+        key = "ABBA"
+        plain_text = "LISAGÅRTILSKOLEN"
+        cipher_text = vigenere.encrypt(key, plain_text)
+        self.assertEqual("LJTAGASTIMTKOMFN", cipher_text)
 
-
-
+    def test_decrypt(self):
+        vigenere = VigenereCipher.norwegian_vigenere()
+        key = "ABBA"
+        plain_text = "LISAGÅRTILSKOLEN"
+        cipher_text = vigenere.encrypt(key, plain_text)
+        decrypted_text = vigenere.decrypt(key, cipher_text)
+        self.assertEqual("LISAGÅRTILSKOLEN", decrypted_text)
